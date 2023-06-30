b3b0107ea48048357e6e8167cae797aa9e925b66
“tamerajaj” <tamer.ajaj@gmail.com>
2023-06-30 09:44:49 +0200

    FastAPI and Docker with simplified data.

---
+++ services/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, Float, String
+from database import Base
+
+
+class House(Base):
+    __tablename__ = "houses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(String)
+    price = Column(Float)
+    bedrooms = Column(Integer)
+    bathrooms = Column(Float)
+    sqft_living = Column(Integer)
+    sqft_lot = Column(Integer)
+    floors = Column(Float)
+    waterfront = Column(Float)
+    view = Column(Float)
+    condition = Column(Integer)
+    grade = Column(Integer)
+    sqft_above = Column(Integer)
+    sqft_basement = Column(Float)
+    yr_built = Column(Integer)
+    yr_renovated = Column(Float)
+    zipcode = Column(Integer)
+    lat = Column(Float)
+    long = Column(Float)
+    sqft_living15 = Column(Integer)
+    sqft_lot15 = Column(Integer)
